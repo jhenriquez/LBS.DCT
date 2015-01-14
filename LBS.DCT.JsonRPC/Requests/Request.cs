1573e505ac0a7272fd22088cb82388128a64f178
Dionisio Ripoll <dripoll@racowireless.com>
2015-01-14 15:08:40 -0400

    Added abstract request class.

---
+++ LBS.DCT.JsonRPC/Requests/Request.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LBS.DCT.JsonRPC.Requests
+{
+    public abstract class Request
+    {
+        public Request(string url, string id)
+        {
+            Client = new WebClient();
+            
+            if (String.IsNullOrEmpty(id))
+            {
+                throw new ArgumentException("id: can not be null or empty.");
+            }
+
+            if (String.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("id: can not be null or empty.");
+            }
+
+            Id = id;
+            Url = url;
+        }
+
+        private WebClient Client { get; set;  }
+        public string Id { get; private set; }
+        public string Url { get; private set; }
+        protected  string Method { get; set; }
+    }
+}
