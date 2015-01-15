36e6b5f5eba5e384b3c7ceb66f83f393e7070742
Julio Henriquez <julio.m.henriquez@gmail.com>
2015-01-15 10:06:00 -0400

    Completed TestKeyRequest and its tests.

--- LBS.DCT.JsonRPC/Requests/TestKeyRequest.cs
+++ LBS.DCT.JsonRPC/Requests/TestKeyRequest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json.Linq;
 
 namespace LBS.DCT.JsonRPC.Requests
 {
@@ -7,5 +8,17 @@ namespace LBS.DCT.JsonRPC.Requests
         public String SessionKey { get; set; }
 
         public TestKeyRequest(String url) : base(url, Guid.NewGuid().ToString()) { Method = "testKey"; }
+
+        public override dynamic Execute()
+        {
+            if (String.IsNullOrEmpty(SessionKey))
+            {
+                throw new InvalidOperationException("SessionKey was not provided.");
+            }
+
+            Parameters = new JArray(new [] { JValue.CreateString(SessionKey) });
+
+            return base.Execute();
+        }
     }
 }
