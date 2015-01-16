92aec84b8443b94cb275808ddc8ddd6c803bfc2f
Julio Henriquez <julio.m.henriquez@gmail.com>
2015-01-16 11:06:38 -0400

    Added Authentication Helper. Missing Async calls.

---
+++ LBS.DCT.JsonRPC.Tests/Helpers/AuthenticatorTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Configuration;
+using LBS.DCT.JsonRPC.Helpers;
+using LBS.DCT.JsonRPC.Requests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LBS.DCT.JsonRPC.Tests.Helpers
+{
+    [TestClass]
+    public class AuthenticatorTest
+    {
+        private Authenticator _testSubject;
+        private String url = "https://pegasus1.pegasusgateway.com/rpc/";
+        private TestKeyRequest _testKeyRequest;
+        
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            _testSubject = new Authenticator(url, ConfigurationManager.AppSettings["SecretKey"]);
+        }
+
+        [TestMethod]
+        public void constructor_defaults_Hash_should_be_null()
+        {
+            Assert.IsNull(_testSubject.Hash);
+        }
+
+        [TestMethod]
+        public void constructor_can_take_a_Hash_Function_value()
+        {
+            _testSubject = new Authenticator(url, (x) =>  "Hassh");
+            Assert.IsNotNull(_testSubject.Hash);
+        }
+
+        [TestMethod]
+        public void constructor_can_take_a_SecretKey_value()
+        {
+            _testSubject = new Authenticator(url, "SecretKey");
+            Assert.IsNotNull(_testSubject.SecretKey);
+            Assert.AreEqual("SecretKey", _testSubject.SecretKey);
+        }
+
+        [TestMethod]
+        public void execute_should_set_the_Challenge_property_along_the_way()
+        {
+            Assert.IsTrue(
+                string.IsNullOrEmpty(_testSubject.Challenge)
+                );
+
+            _testSubject.Execute();
+
+            Assert.IsFalse(
+                string.IsNullOrEmpty(_testSubject.Challenge)
+                );
+        }
+
+        [TestMethod]
+        public void execute_should_return_a_valid_key()
+        {
+            var execute = _testSubject.Execute();
+
+            Assert.IsFalse(String.IsNullOrEmpty(execute));
+
+            _testKeyRequest = new TestKeyRequest(url);
+            _testKeyRequest.SessionKey = execute;
+
+            Assert.AreEqual("ok", _testKeyRequest.Execute().result.ToString());
+        }
+    }
+}
---
+++ LBS.DCT.JsonRPC/Helpers/Authenticator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using LBS.DCT.JsonRPC.Requests;
+
+namespace LBS.DCT.JsonRPC.Helpers
+{
+    public class Authenticator
+    {
+        private ChallengeRequest ChallengeRequest { get; set; }
+        private SessionKeyRequest SessionKeyRequest { get; set; }
+        private Action<String> AsyncCallback { get; set; }
+
+        public Func<String, String> Hash { get; set; }
+        public String SecretKey { get; set; }
+        public String Challenge { get; set; }
+
+        public Authenticator(String url)
+        {
+            ChallengeRequest = new ChallengeRequest(url);
+            SessionKeyRequest = new SessionKeyRequest(url);
+        }
+
+        public Authenticator(String url, String secret)
+            : this(url)
+        {
+            SecretKey = secret;
+        }
+
+        public Authenticator(String url, Func<String, String> hasher)
+            : this(url)
+        {
+            Hash = hasher;
+        }
+
+        public String Execute()
+        {
+            Challenge = SessionKeyRequest.Challenge = ChallengeRequest.Execute().result.ToString();
+
+            SessionKeyRequest.HashedSecret = Hash != null ?  Hash(Challenge) : internalHasher();
+
+            return SessionKeyRequest.Execute().result.ToString();
+        }
+
+        public void ExecuteAsync(Action<String> callback)
+        {
+        }
+
+        private String internalHasher()
+        {
+            var crypto = new SHA1CryptoServiceProvider();
+            var secretKey = crypto.ComputeHash(Encoding.ASCII.GetBytes(String.Format("{0}{1}", Challenge, SecretKey)));
+            return BitConverter.ToString(secretKey).Replace("-", "").ToLower();
+        }
+    }
+}
