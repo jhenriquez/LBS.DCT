12893dda46af7cc5d861327f4a1248da7fc53954
Julio Henriquez <julio.m.henriquez@gmail.com>
2015-01-19 10:11:38 -0400

    Added project description. nuspec created.

--- Seiya.JsonRPC/Properties/AssemblyInfo.cs
+++ Seiya.JsonRPC/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Seiya.JsonRPC")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library to access the DCT (Pegasus Gateway) RPC web service.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Seiya.JsonRPC")]
